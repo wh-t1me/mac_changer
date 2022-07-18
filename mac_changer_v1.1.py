0206b6e0f96af11cdd6511f0301e9dff91296ab4
wh-t1me <zharochich5999@gmail.com>
2022-07-18 16:35:59 +0400

    mac_changer_v1.1.py

---
+++ mac_changer_v1.1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import subprocess
+a = input("""Enter mac address 
+E.g. 00:11:22:33:44:55: """)
+subprocess.call("ifconfig eth0 down" , shell = True)
+subprocess.call("ifconfig eth0 hw ether " + a, shell = True)
+subprocess.call("ifconfig eth0 up" , shell = True)
